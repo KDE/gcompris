0039fd4f5dbc1d67faaff425dde4fb940121c92f
Timothée Giet <animtim@gmail.com>
2026-07-07 12:22:34 +0200

    core, improve synth preset
    - Use shorter enveloppe values.
    - Reduce peak amplitude.
    - Reduce number of harmonics and balance them better.
    - Add a little harmonics phase offset for better result.

--- src/core/synth/preset.cpp
+++ src/core/synth/preset.cpp
@@ -6,20 +6,21 @@
 
 #include "preset.h"
 
-Preset::Preset() : timbreAmplitudes(8, 0), timbrePhases(8, 0) {
+Preset::Preset() : timbreAmplitudes(4, 0), timbrePhases(4, 0) {
 //hardcode custom church pad preset
     waveformMode = Waveform::MODE_SIN;
     env.attackTime = 50;
-    env.decayTime = 400;
-    env.releaseTime = 100;
+    env.decayTime = 50;
+    env.releaseTime = 10;
 
     env.initialAmpl = 0;
-    env.peakAmpl = 1;
+    env.peakAmpl = 0.9;
     env.sustainAmpl = 0.8;
 
-    timbreAmplitudes[0] = 50;
-    timbreAmplitudes[1] = 15;
-    timbreAmplitudes[3] = 15;
-    timbreAmplitudes[7] = 50;
+    timbreAmplitudes[0] = 40;
+    timbreAmplitudes[1] = 20;
+    timbreAmplitudes[3] = 10;
     timbrePhases[0] = 0;
+    timbrePhases[1] = 10;
+    timbrePhases[3] = 20;
 }
